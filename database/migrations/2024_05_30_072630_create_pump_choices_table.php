b1bccd54536d7e5611691be89a8ec8e5385945f1
FarhadKamal <jitubd1971@gmail.com>
2024-05-30 14:49:24 +0600

    jitu-changed

---
+++ database/migrations/2024_05_30_072630_create_pump_choices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePumpChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pump_choices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('lead_id');
+            $table->integer('req_id');
+            $table->integer('product_id');
+            $table->double('pump_head')->default(0);
+            $table->double('unit_price')->default(0);
+            $table->double('qty');
+            $table->integer('discount_price')->default(0);
+            $table->integer('net_price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pump_choices');
+    }
+}

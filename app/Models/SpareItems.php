e7f2fd7d01321d41c382684de0c2e3dcea42147e
FarhadKamal <jitubd1971@gmail.com>
2024-06-29 12:32:07 +0600

    r

---
+++ app/Models/SpareItems.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpareItems extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'old_code',
+        'new_code',
+        'mat_name',
+        'brand_name'
+
+    ];
+}
---
+++ database/migrations/2024_06_29_062044_create_spare_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpareItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spare_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('old_code')->nullable();
+            $table->integer('new_code');
+            $table->string('mat_name');
+            $table->string('brand_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spare_items');
+    }
+}
